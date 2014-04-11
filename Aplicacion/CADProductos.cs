0b14a7e76759b40c779d541d480486fe7ed58012
vic <victorfz@gmail.com>
2014-04-11 21:32:57 +0200

    commit inicial
    Parte de ENProducto y CAD producto

---
+++ Aplicacion/CADProductos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aplicacion
+{
+    class CADProductos
+    {
+        private string conexion;
+
+        public string Conexion
+        {
+            get { return conexion; }
+            set { conexion = value; }
+        }
+        public ENProductos obtenerProducto(int Id)
+        {
+            ENProductos p= new ENProductos();
+            return p;
+        }
+        public static ENProductos[] obtenerProductos()
+        {
+            ENProductos[] pt=new ENProductos[3];
+            return pt;
+        }
+        public int nuevo(ENProductos p)
+        {
+            ENProductos pn = new ENProductos();
+            return 1;//Si es OK devuelve el ID
+        }
+        public int actualizarProducto()
+        {
+            // ......
+            return 1;//Si es OK devuelve el ID
+        }
+
+
+    }
+}
---
+++ Aplicacion/ENProductos.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aplicacion
+{
+    public class ENProductos
+    {
+        private int IdProducto;
+
+        public int IdProducto1
+        {
+            get { return IdProducto; }
+            set { IdProducto = value; }
+        }
+        private string Nombre_es;
+
+        public string Nombre_es1
+        {
+            get { return Nombre_es; }
+            set { Nombre_es = value; }
+        }
+        private string Nombre_en;
+
+        public string Nombre_en1
+        {
+            get { return Nombre_en; }
+            set { Nombre_en = value; }
+        }
+        private string Descripcion_es;
+
+        public string Descripcion_es1
+        {
+            get { return Descripcion_es; }
+            set { Descripcion_es = value; }
+        }
+        private string Descripcion_en;
+
+        public string Descripcion_en1
+        {
+            get { return Descripcion_en; }
+            set { Descripcion_en = value; }
+        }
+        private float PVP;
+
+        public float PVP1
+        {
+            get { return PVP; }
+            set { PVP = value; }
+        }
+        private int IdFamilia;
+
+        public int IdFamilia1
+        {
+            get { return IdFamilia; }
+            set { IdFamilia = value; }
+        }
+        private int IdMarca;
+
+        public int IdMarca1
+        {
+            get { return IdMarca; }
+            set { IdMarca = value; }
+        }
+        private int Stock;
+
+        public int Stock1
+        {
+            get { return Stock; }
+            set { Stock = value; }
+        }
+        private float Valoracion;
+
+        public float Valoracion1
+        {
+            get { return Valoracion; }
+            set { Valoracion = value; }
+        }
+
+    }
+}
